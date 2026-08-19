6fc57e4d6dc747aeca8592e35fc1c76e2a5e73eb
@algoreadme <dhikaaditya1@gmail.com>
2026-08-19 11:35:24 +0700

    chore: create xcodeproj with SwiftUI and remove unused file for now

---
+++ TapCash/TapCash/TapCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapCashApp.swift
+//  TapCash
+//
+//  Created by DHIKA ADITYA ARE on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct TapCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
